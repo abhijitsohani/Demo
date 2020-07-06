3ec011872cf7f262ac1889f7f2b1c1a0bbc33c07
Abhijit Sohani <abhijitsohani@hotmail.com>
2020-07-05 19:16:44 -0500

    FirstWS first commit.

---
+++ FirstWS/src/test/java/FirstWSTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FirstWSTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		// fail("ERROR");
+		// throw new Exception("ERROR!");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertTrue("Implemented", true);
+	}
+
+}
